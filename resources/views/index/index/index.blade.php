366429e4381da6db33864a7e7ace26802dc073cf
meetmr <83466349@qq.com>
2018-08-15 14:44:30 +0800

    搭建前台页面

---
+++ resources/views/index/index/index.blade.php
@@ -0,0 +1,287 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+<title>爱心社 - 四川信息职业技术学院</title>
+<link rel="stylesheet" type="text/css" href="{{ asset('/index/css/pure-min.css')}}">
+<link rel="stylesheet" type="text/css" href="{{ asset('/index/css/grids-responsive-min.css')}}">
+<link rel="stylesheet" type="text/css" href="{{ asset('/index/css/global.css')}}">
+</head>
+<body class="page-index">
+
+<div class="wraper-bg">
+
+	<div class="header">
+		<div class="home-menu pure-menu pure-menu-horizontal pure-menu-fixed">
+
+			<a class="logo-set pure-menu-heading" href="./index.jsp">
+				<img class="logo" src="/index/picture/logo.png">
+			</a>
+
+			<a class="title-set pure-menu-heading" href="./index.jsp">
+				<span class="big-text">爱心社</span>
+				<span class="small-text">Love society</span>
+			</a>
+
+			<ul class="home-menu-list pure-menu-list">
+				<li class="pure-menu-item"><a href="words.jsp" class="pure-menu-link">留言板</a></li>
+						<li class="pure-menu-item"><a class="pure-menu-link" href="./login.jsp">登陆/注册</a></li>
+			</ul>
+		</div>
+	</div>
+
+	<div class="splash-wrap">
+		<div class="splash">
+			<div class="splash-head pure-g">
+
+				<div class="logo pure-u-1 pure-u-md-3-8">
+					<img class="logo" src="/index/picture/logo.png">
+				</div>
+
+				<div class="text pure-u-1 pure-u-md-5-8">
+					<div>
+						<p class="centen1">
+							<img src="/index/picture/ji.svg"><img src="/index/picture/jin.svg"><img src="/index/picture/fang.svg"><img src="/index/picture/cun.svg"><img src="/index/picture/rong.svg"><img src="/index/picture/tian.svg"><img src="/index/picture/xia.svg">
+						</p>
+						<p class="centen2">
+							<img src="/index/picture/gong.svg"><img src="/index/picture/ju.svg"><img src="/index/picture/jian.svg"><img src="/index/picture/pan.svg"><img src="/index/picture/shu.svg"><img src="/index/picture/hui.svg"><img src="/index/picture/huang.svg">
+						</p>
+						<p class="enters">
+							<a href="action.jsp" class="pure-button pure-button-primary">活动中心</a>
+							<a href="about.html" class="pure-button pure-button-primary">关于我们 :)</a>
+						</p>
+					</div>
+				</div>
+			</div>
+
+			<p class="splash-subhead">
+				The world is boring, write the future together by f12;
+			</p>
+
+		</div>
+	</div>
+
+	<div class="hyperlinks-wrap">
+		<div class="hyperlinks pure-g">
+			<span class="pure-u-1">
+				<a href="//scitc.com.cn/">
+					<img src="/index/picture/reduce-red.png">
+				</a>
+			</span>
+		</div>
+	</div>
+
+	<div class="box-content-wraper">
+		<div class="box-list">
+
+			<div class="item pure-g">
+				<div class="info pure-u-1-1 pure-u-md-3-24">
+					<div class="time">2017-10-12 00:00</div>
+					<div class="type">竞赛</div>
+				</div>
+				<div class="content pure-u-1-1 pure-u-md-19-24">
+					<div class="title">5分钟个人演讲 ——大学对我的改变</div>
+					<div class="sub">5分钟个人演讲
+						——大学对我的改变
+						10月12日上午——大学对我的改变
+						10月12日上午——大学对我的改变
+						10月12日上午——大学对我的改变
+						10月12日上午</div>
+					<a  id="buttt" href="./detail.jsp?activityID=4087E313965240ECAF732460BC60E303" class="pure-button pure-input-1 pure-button-primary">活动详情</a>
+				</div>
+			</div>
+
+		</div>
+		<div class="box-list">
+
+			<div class="item pure-g">
+				<div class="info pure-u-1-1 pure-u-md-3-24">
+					<div class="time">2017-10-12 00:00</div>
+					<div class="type">竞赛</div>
+				</div>
+				<div class="content pure-u-1-1 pure-u-md-19-24">
+					<div class="title">5分钟个人演讲 ——大学对我的改变</div>
+					<div class="sub">5分钟个人演讲
+						——大学对我的改变
+
+						10月12日上午</div>
+					<a  id="buttt" href="./detail.jsp?activityID=4087E313965240ECAF732460BC60E303" class="pure-button pure-input-1 pure-button-primary">活动详情</a>
+				</div>
+			</div>
+
+		</div>
+		<div class="box-list">
+
+			<div class="item pure-g">
+				<div class="info pure-u-1-1 pure-u-md-3-24">
+					<div class="time">2017-10-12 00:00</div>
+					<div class="type">竞赛</div>
+				</div>
+				<div class="content pure-u-1-1 pure-u-md-19-24">
+					<div class="title">5分钟个人演讲 ——大学对我的改变</div>
+					<div class="sub">5分钟个人演讲
+						——大学对我的改变
+
+						10月12日上午</div>
+					<a  id="buttt" href="./detail.jsp?activityID=4087E313965240ECAF732460BC60E303" class="pure-button pure-input-1 pure-button-primary">活动详情</a>
+				</div>
+			</div>
+
+		</div>
+		<div class="box-list">
+
+			<div class="item pure-g">
+				<div class="info pure-u-1-1 pure-u-md-3-24">
+					<div class="time">2017-10-12 00:00</div>
+					<div class="type">竞赛</div>
+				</div>
+				<div class="content pure-u-1-1 pure-u-md-19-24">
+					<div class="title">5分钟个人演讲 ——大学对我的改变</div>
+					<div class="sub">5分钟个人演讲
+						——大学对我的改变
+
+						10月12日上午</div>
+					<a  id="buttt" href="./detail.jsp?activityID=4087E313965240ECAF732460BC60E303" class="pure-button pure-input-1 pure-button-primary">活动详情</a>
+				</div>
+			</div>
+
+		</div>
+		<div class="box-list">
+
+			<div class="item pure-g">
+				<div class="info pure-u-1-1 pure-u-md-3-24">
+					<div class="time">2017-10-12 00:00</div>
+					<div class="type">竞赛</div>
+				</div>
+				<div class="content pure-u-1-1 pure-u-md-19-24">
+					<div class="title">5分钟个人演讲 ——大学对我的改变</div>
+					<div class="sub">5分钟个人演讲
+						——大学对我的改变
+
+						10月12日上午</div>
+					<a  id="buttt" href="./detail.jsp?activityID=4087E313965240ECAF732460BC60E303" class="pure-button pure-input-1 pure-button-primary">活动详情</a>
+				</div>
+			</div>
+
+		</div>
+		<div class="box-list">
+
+			<div class="item pure-g">
+				<div class="info pure-u-1-1 pure-u-md-3-24">
+					<div class="time">2017-10-12 00:00</div>
+					<div class="type">竞赛</div>
+				</div>
+				<div class="content pure-u-1-1 pure-u-md-19-24">
+					<div class="title">5分钟个人演讲 ——大学对我的改变</div>
+					<div class="sub">5分钟个人演讲
+						——大学对我的改变
+
+						10月12日上午</div>
+					<a  id="buttt" href="./detail.jsp?activityID=4087E313965240ECAF732460BC60E303" class="pure-button pure-input-1 pure-button-primary">活动详情</a>
+				</div>
+			</div>
+
+		</div>
+		<div class="box-list">
+
+			<div class="item pure-g">
+				<div class="info pure-u-1-1 pure-u-md-3-24">
+					<div class="time">2017-10-12 00:00</div>
+					<div class="type">竞赛</div>
+				</div>
+				<div class="content pure-u-1-1 pure-u-md-19-24">
+					<div class="title">5分钟个人演讲 ——大学对我的改变</div>
+					<div class="sub">5分钟个人演讲
+						——大学对我的改变
+
+						10月12日上午</div>
+					<a  id="buttt" href="./detail.jsp?activityID=4087E313965240ECAF732460BC60E303" class="pure-button pure-input-1 pure-button-primary">活动详情</a>
+				</div>
+			</div>
+
+		</div>
+		<div class="box-list">
+
+			<div class="item pure-g">
+				<div class="info pure-u-1-1 pure-u-md-3-24">
+					<div class="time">2017-10-12 00:00</div>
+					<div class="type">竞赛</div>
+				</div>
+				<div class="content pure-u-1-1 pure-u-md-19-24">
+					<div class="title">5分钟个人演讲 ——大学对我的改变</div>
+					<div class="sub">5分钟个人演讲
+						——大学对我的改变
+
+						10月12日上午</div>
+					<a  id="buttt" href="./detail.jsp?activityID=4087E313965240ECAF732460BC60E303" class="pure-button pure-input-1 pure-button-primary">活动详情</a>
+				</div>
+			</div>
+
+		</div>
+		<div class="box-list">
+
+			<div class="item pure-g">
+				<div class="info pure-u-1-1 pure-u-md-3-24">
+					<div class="time">2017-10-12 00:00</div>
+					<div class="type">竞赛</div>
+				</div>
+				<div class="content pure-u-1-1 pure-u-md-19-24">
+					<div class="title">5分钟个人演讲 ——大学对我的改变</div>
+					<div class="sub">5分钟个人演讲
+						——大学对我的改变
+
+						10月12日上午</div>
+					<a  id="buttt" href="./detail.jsp?activityID=4087E313965240ECAF732460BC60E303" class="pure-button pure-input-1 pure-button-primary">活动详情</a>
+				</div>
+			</div>
+
+		</div>
+		<div class="box-list">
+
+			<div class="item pure-g">
+				<div class="info pure-u-1-1 pure-u-md-3-24">
+					<div class="time">2017-10-12 00:00</div>
+					<div class="type">竞赛</div>
+				</div>
+				<div class="content pure-u-1-1 pure-u-md-19-24">
+					<div class="title">5分钟个人演讲 ——大学对我的改变</div>
+					<div class="sub">5分钟个人演讲
+						——大学对我的改变
+
+						10月12日上午</div>
+					<a  id="buttt" href="./detail.jsp?activityID=4087E313965240ECAF732460BC60E303" class="pure-button pure-input-1 pure-button-primary">活动详情</a>
+				</div>
+			</div>
+
+		</div>
+		<div class="box-list">
+
+			<div class="item pure-g">
+				<div class="info pure-u-1-1 pure-u-md-3-24">
+					<div class="time">2017-10-12 00:00</div>
+					<div class="type">竞赛</div>
+				</div>
+				<div class="content pure-u-1-1 pure-u-md-19-24">
+					<div class="title">5分钟个人演讲 ——大学对我的改变</div>
+					<div class="sub">5分钟个人演讲
+						——大学对我的改变
+
+						10月12日上午</div>
+					<a  id="buttt" href="./detail.jsp?activityID=4087E313965240ECAF732460BC60E303" class="pure-button pure-input-1 pure-button-primary">活动详情</a>
+				</div>
+			</div>
+
+		</div>
+	</div>
+</div>
+
+<div class="footer">
+	<div class="text">
+		<p>四川信息职业技术学院 - 爱心社</p>
+		<p>Copyright © 2018 SCITC - Love society</p>
+	</div>
+</div>
+
+</body>
+</html>
